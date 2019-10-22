32842f8a71e25b93476f99ce9b5bd35860f5cacd
Mia Pan <miapanyuewei@gmail.com>
2019-10-22 00:27:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,11 +23,11 @@ def monopoly_with_third_tier
     name: {}
       
       
-    rent_in_dollars:{
-      :one_piece_owned =>25,
-      :two_pieces_owne =>50,
-      :three_pieces_owned =>100,
-      :four_pieces_owned =>200
+    rent_in_dollars: {
+      :one_piece_owned => 25,
+      :two_pieces_owne => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
        }
     }
   # When you start writing the implementation for this method, copy the Hash
@@ -41,16 +41,16 @@ def monopoly_with_fourth_tier
     pieces: 4,
     name: {
       :reading_railroad => {"mortgage_value": "$100"},
-      :pennsylvania_railroad =>{"mortgage_value": "$200"},
-      :b_and_o_railroad =>{"mortgage_value":"$400"},
-      :shortline_railroad =>{"mortgage_value":"$800" }
+      :pennsylvania_railroad => {"mortgage_value": "$200"},
+      :b_and_o_railroad =>{ "mortgage_value":"$400"},
+      :shortline_railroad => {"mortgage_value":"$800" }
       }
       
     rent_in_dollars:{
-      :one_piece_owned =>25,
-      :two_pieces_owne =>50,
-      :three_pieces_owned =>100,
-      :four_pieces_owned =>200
+      :one_piece_owned => 25,
+      :two_pieces_owne => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
        }
     }
   # When you start writing the implementation for this method, copy the Hash
